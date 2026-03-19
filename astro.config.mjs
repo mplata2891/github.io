1cb2ee6335b591aa6674c26efc58bd985bd72845
M1-K3 <ninjastudies@gmail.com>
2026-03-19 01:21:25 -0700

    adding base as github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   // 1. INJECT YOUR GITHUB PAGES URL HERE
   site: 'https://mplata2891.github.io',
-
-  // 2. REPOSITORY ROUTING (Read carefully)
-  // If your GitHub repository is named EXACTLY "<YOUR_USERNAME>.github.io", leave this commented out.
-  // If your repository is named something else (like "portfolio" or "m1-k3"), uncomment this line and put the repo name here:
-  // base: '/<YOUR_REPO_NAME>',
+  base: '/github.io',
 
   vite: {
     plugins: [tailwindcss()]
